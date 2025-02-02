cdc8b62238e459aa17ed8ed00ecb40ebd0469f82
Karthik Th <karthik@kloudlite.io>
2025-02-02 13:51:43 +0530

    db setup

---
+++ src/orm/index.ts
@@ -0,0 +1,18 @@
+import { DataSource } from "typeorm";
+import { User } from "./entities/user";
+
+const AppDataSource = new DataSource({
+  type: "postgres",
+  host: process.env.DB_HOST,
+  port: parseInt(process.env.DB_PORT||"5432"),
+  username: process.env.DB_USERNAME,
+  password: process.env.DB_PASSWORD,
+  database: process.env.DB_DATABASE,
+  synchronize: true,
+  entities: [User],
+  logging: false,
+});
+
+await AppDataSource.initialize();
+
+export const UserRepo = AppDataSource.getRepository(User);
